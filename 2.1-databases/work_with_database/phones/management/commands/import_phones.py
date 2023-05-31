fbd99382b4991bca96e168d2d49ee9b1134819c7
Almaz Galimov <almazpython@gmail.com>
2023-05-31 14:21:30 +0500

    сделал импорт csv файла в базу

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,24 @@
 import csv
-
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
 from phones.models import Phone
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
-
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            id = phone['id']
+            name = phone['name']
+            price = phone['price']
+            image = phone['image']
+            release_date = phone['release_date']
+            lte_exists = phone['lte_exists']
+            slug = slugify(name)
+
+            phone_obj = Phone(id=id, name=name, price=price, image=image, release_date=release_date, lte_exists=lte_exists,
+                              slug=slug)
+            phone_obj.save()
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(u'Название', max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='phone_images/')
+    release_date = models.DateField(u'Дата релиза')
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('name', kwargs={'slug': self.slug})
