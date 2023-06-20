3d323ca7cecff8871b08814c75b2a1c9d4d64183
Almaz Galimov <almazpython@gmail.com>
2023-06-20 10:55:30 +0500

    all done

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Temperature: {self.temperature} (Sensor: {self.sensor})'
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import SensorListCreateView, SensorRetrieveUpdateDestroyView, MeasurementCreateView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorListCreateView.as_view(), name='sensor-list'),
+    path('sensors/<int:pk>/', SensorRetrieveUpdateDestroyView.as_view(), name='sensor-detail'),
+    path('measurements/', MeasurementCreateView.as_view(), name='measurement-create'),
 ]
